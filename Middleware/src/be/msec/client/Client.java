6ca210c1390b94719065107ff732fc6b73557024
Bruno Rocha Pereira <b.rocha.per@gmail.com>
2017-05-21 10:52:19 +0200

    [Add-WIP] First authentication step

--- Middleware/src/be/msec/client/Client.java
+++ Middleware/src/be/msec/client/Client.java
@@ -4,9 +4,23 @@ import be.msec.client.connection.Connection;
 import be.msec.client.connection.IConnection;
 import be.msec.client.connection.SimulatedConnection;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 import javax.smartcardio.*;
+
 
 public class Client {
 
@@ -14,12 +28,81 @@ public class Client {
 	private static final byte VALIDATE_PIN_INS = 0x22;
 	private static final byte GET_SERIAL_INS = 0x24;
 	private static final byte SIGN_DATA = 0x26;
-	private static final byte UPDATE_TIME = 0x28;
+	private static final byte ECHO = 0x28;
+	private static final byte VALIDATE_TIME = 0x30;
 	
 	private final static short SW_VERIFICATION_FAILED = 0x6300;
 	private final static short SW_PIN_VERIFICATION_REQUIRED = 0x6301;
 	
+
 	
+	private static void authenticate(CommandAPDU a, ResponseAPDU r, IConnection c) throws Exception{
+		if (!validate_Time(a, r, c)){
+			System.out.println("NEED VALIDATION");
+	        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+	        Socket clientSocket = new Socket("localhost", 8000);
+	        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+	        DataInputStream dIn = new DataInputStream(clientSocket.getInputStream());
+	        outToServer.writeByte(1);
+	        
+	        byte[] length = new byte[4];
+	        length[0] = (byte) dIn.read(); 
+	        length[1] = (byte) dIn.read();
+	        length[2] = (byte) dIn.read();
+	        length[3] = (byte) dIn.read();
+	        int len = length[0] << 24 | (length[1] & 0xFF) << 16 | (length[2] & 0xFF) << 8 | (length[3] & 0xFF);
+	        byte[] message = null;// read length of incoming message
+	        
+	        if(len>0) {
+	        	System.out.println(len);
+	            message = new byte[len];
+	            dIn.readFully(message, 0, message.length); // read the message
+	        }
+	        System.out.println(message.length + " bytes read.");
+	        
+	        clientSocket.close();
+		}
+		
+		
+		// authenticateSP
+		// authenticateCard
+		// releaseAttributes
+	}
+	private static boolean validate_Time(CommandAPDU a, ResponseAPDU r, IConnection c) throws Exception {
+		// TODO Auto-generated method stub
+		int dataLen = 8;
+		Date currentTime = new Date();
+		LocalDateTime currentTimeDate = currentTime.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		byte[] yearBytes = ByteBuffer.allocate(4).putInt(currentTimeDate.getYear()).array();
+		byte[] dateBytes = new byte[8];
+		System.arraycopy(yearBytes, 0, dateBytes, 0, 4);
+		dateBytes[4] = (byte) currentTimeDate.getMonthValue();
+		dateBytes[5] = (byte) currentTimeDate.getDayOfMonth();
+		dateBytes[6] = (byte) currentTimeDate.getHour();
+		dateBytes[7] = (byte) currentTimeDate.getMinute();
+		a = new CommandAPDU(IDENTITY_CARD_CLA, VALIDATE_TIME, 0x00, 0x00,dateBytes);
+		byte[] dataIn = Arrays.copyOfRange(a.getBytes(), 0x05, 5 + 100); 
+		System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(dataIn));
+		r = c.transmit(a);
+		byte[] date = Arrays.copyOfRange(dataIn, (short)0x00, (short)4);
+		int b = date[0] << 24 | (date[1] & 0xFF) << 16 | (date[2] & 0xFF) << 8 | (date[3] & 0xFF);
+		System.out.println(b);
+		
+		byte[] dataOut = Arrays.copyOfRange(r.getData(),(short) 5 + dataLen, 5 + dataLen+1); 
+		int response = dataOut[0];
+		System.out.println(response);
+		if (response == 1) {
+			return false;
+		} else {
+			return true;
+		}
+		
+//		byte[] dateAfter = Arrays.copyOfRange(dataOut, (short)0x00, (short)4);
+//		int d = dateAfter[0] << 24 | (dateAfter[1] & 0xFF) << 16 | (dateAfter[2] & 0xFF) << 8 | (dateAfter[3] & 0xFF);
+//		System.out.println(d);
+		
+	}
 	/**
 	 * @param args
 	 */
@@ -45,8 +128,8 @@ public class Client {
 			 * See http://java.sun.com/javase/6/docs/jre/api/security/smartcardio/spec/index.html
 			 */
 			
-			CommandAPDU a;
-			ResponseAPDU r;
+			CommandAPDU a = null;
+			ResponseAPDU r = null;
 			//CommandAPDU(int class,int instruction,bytes param1,bytes param2,int data)
 			
 			//getSW: 0x9000 correct, 0x9001 incorrect
@@ -71,15 +154,7 @@ public class Client {
 			}
 			
 			//2. Send PIN
-			int dataLen = 5;
-			a = new CommandAPDU(IDENTITY_CARD_CLA, UPDATE_TIME, 0x00, 0x00,new byte[]{0x01,0x02,0x03,0x04,0x05});
-			byte[] dataIn = Arrays.copyOfRange(a.getBytes(), 0x05, 5 + dataLen); 
-			System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(dataIn));
-			r = c.transmit(a);
-			
-			byte[] dataOut = Arrays.copyOfRange(r.getBytes(), 5, 5 + dataLen); 
-
-			System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(dataOut));
+			authenticate(a,r,c);
 //			if (r.getSW()==SW_VERIFICATION_FAILED) throw new Exception("PIN INVALID");
 //			else if(r.getSW()!=0x9000) throw new Exception("Exception on the card: " + r.getSW());
 //			System.out.println("PIN Verified");
